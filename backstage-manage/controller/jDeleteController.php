8f88668c733eebe06b0812fcb79ac20f371f00be
Ji <1071163919@qq.com>
2017-03-29 20:18:44 +0800

    modify packaging imgupload & imgdel function

--- backstage-manage/controller/jDeleteController.php
+++ backstage-manage/controller/jDeleteController.php
@@ -1,12 +1,4 @@
 <?php
-  require_once '../../include.php';
-
+  require_once 'delImg.php';
   $id = $_GET['id'];
-  $path = fetchOne('select * from jkp where id = '.$id)['imgpath'];
-  $rPath = str_replace('http://jiduo.org', '', $path);
-  $pPath = ROOT.$rPath;
-  if (delete('jkp', 'id = '.$id) && unlink($pPath)) {
-    echo "<script>alert('删除成功');window.location.href='../mr-long.php';</script>";
-  } else {
-    echo "<script>alert('删除失败');</script>";
-  }
+  deleteImg('jkp', $id);
